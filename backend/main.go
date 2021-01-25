51c35f043c207f5b79fc58a481d02169513eaf6f
shashank.g172 <shashank.g172@gmail.com>
2021-01-25 08:54:52 -0500

    Multiple clients example

--- backend/main.go
+++ backend/main.go
@@ -3,62 +3,37 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"log"
-	"github.com/gorilla/websocket"
+
+	"github.com/shashankg172/go-chat/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-  	WriteBufferSize: 1024,
-
-  // Check the origin of our connection
-  // Make requests from React  
-  // any connection is allowed for now  
-  CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// Listen for new messages sent to Websocket Endpoint
-func reader(conn *websocket.Conn) {
-    for {
-    // read in a message
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println(err)
-            return
-        }
-    // print out that message for clarity
-        fmt.Println(string(p))
-
-        if err := conn.WriteMessage(messageType, p); err != nil {
-            log.Println(err)
-            return
-        }
-
-    }
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := upgrader.Upgrade(w,r, nil)
-	if err!=nil {
-		log.Println(err)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
 	}
 
-	// listen indefinitely
-	reader(ws)
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "Simple Server")
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
 	})
-	http.HandleFunc("/ws", serveWs)
 }
 
-
 func main() {
-	fmt.Println("Chat v0.01")
+	fmt.Println("Distributed Chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
